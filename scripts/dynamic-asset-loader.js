a8a9aefee7e97925dfb7bb2db6c723b0dd9264a1
iOmega8561 <giusepperocco38@gmail.com>
2024-11-18 20:36:49 +0100

    Loaded assets for light and dark mode, applied script to the rest of the pages

--- scripts/dynamic-asset-loader.js
+++ scripts/dynamic-asset-loader.js
@@ -4,14 +4,17 @@ function updateAssetsForTheme(theme) {
 
     assets.forEach(asset => {
         const filename = asset.getAttribute('data-theme-asset');
-        const newPath = `./assets/${theme}/${filename}`;
+        const newPath = `../assets/${theme}/${filename}`;
         asset.setAttribute('src', newPath);
     });
 }
 
 const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-updateAssetsForTheme(isDarkMode ? 'dark' : 'light');
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
     updateAssetsForTheme(event.matches ? 'dark' : 'light');
 });
+
+window.onload = function() {
+    updateAssetsForTheme(isDarkMode ? 'dark' : 'light');
+}
